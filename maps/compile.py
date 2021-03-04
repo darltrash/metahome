e2f6aa5d72240c245637c993e472a070ce06b074
Darltrash <darlex259@gmail.com>
2021-03-04 00:48:23 -0300

    Fixed the collision thingy and cleared some things in the README

--- maps/compile.py
+++ maps/compile.py
@@ -17,11 +17,19 @@ for level in data["levels"]:
     collisionHash = {}
     tiles = []
 
+    # Before anyone says "hEY BuT WHat iF U uSe diFFeRenT sIzeS PeR tIlesET LayER??!"
+    # You need to consider this was made for metahome only, and this will get refined over time
+
+    width = 0
+    gridsize = 0
     for layer in level["layerInstances"]:
+        width = layer["__cWid"]
         if layer["__type"] == "IntGrid":
             for intgrid in layer["intGrid"]:
                 collisionHash[intgrid["coordId"]] = True
+                print(intgrid["coordId"])
         elif layer["__type"] == "Tiles":
+            gridsize = layer["__gridSize"]
             for tile in layer["gridTiles"]:
                 tiles.append(tile)
 
@@ -30,7 +38,7 @@ for level in data["levels"]:
         _tile["x"] = tile["px"][0]
         _tile["y"] = tile["px"][1]
 
-        _tile["c"] = collisionHash.get(tile["px"][0]+tile["px"][1], False)
+        _tile["c"] = collisionHash.get((tile["px"][0]/gridsize) + (width * (tile["px"][1]/gridsize)), False)
 
         _tile["sx"] = tile["src"][0]
         _tile["sy"] = tile["src"][1]
